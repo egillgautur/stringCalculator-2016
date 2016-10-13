ab36d99f263867d29dd9d3ed349ce9c3e6c3d2bc
Egill Gautur Steingrímsson <Egill Gautur Steingrmsson>
2016-10-13 19:07:32 +0000

    testEmptyString OK

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,12 +1,16 @@
 package is.ru.stringcalculator;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-
-	return 0;
+    public static int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        } else {
+            return Integer.parseInt(numbers);
+        }
+    }
 }
 
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -1,10 +1,13 @@
 package is.ru.stringcalculator;
-
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.Rule;
 
 public class CalculatorTest {
-    return 0;
+    
+    @Test
+	public void testEmptyString() {
+		assertEquals(0, Calculator.add(""));
+	}
 }
