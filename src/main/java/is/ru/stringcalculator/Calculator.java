b824ecb703cc1db1d246e484edcbe28e92125596
Egill Gautur Steingrímsson <Egill Gautur Steingrmsson>
2016-10-13 17:58:03 +0000

    src

---
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -0,0 +1,12 @@
+package is.ru.stringcalculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+	
+}
+
---
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,10 @@
+package is.ru.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.Rule;
+
+public class CalculatorTest {
+
+}
